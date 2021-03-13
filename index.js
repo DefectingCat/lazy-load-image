a31ad18ec363bfc653cbeef42f93ce7b9f26c22b
DefectingCat <i@defect.ink>
2021-03-13 14:25:40 +0800

    使用sass

--- index.js
+++ index.js
@@ -75,71 +75,107 @@ function createLi() {
 let ul = createLi();
 document.body.append(ul);
 
+// /////////////内容会抖动///////////////// //
 // 创建图片
-let requestImg = [
-  {
-    src: './img/file_4963928.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963929.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963930.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963931.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963932.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963933.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963934.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963935.webp',
-    w: 450,
-    h: 512,
-  },
-  {
-    src: './img/file_4963936.webp',
-    w: 450,
-    h: 512,
-  },
-];
 function createImg() {
   let frag = document.createDocumentFragment();
   let num = 28;
   for (let i = 0; i < 9; i++) {
     let img = document.createElement('img');
     // placeholder 地址
-    img.src = `./img/placeholder/file_49639${num}.webp`;
+    img.src = './img/placeholder.gif';
     // 图片真实地址
     img.setAttribute('data-src', `./img/file_49639${num}.webp`);
     num++;
     frag.append(img);
   }
+  frag.append(document.createElement('hr'));
   return frag;
 }
 let images = createImg();
 document.body.append(images);
 
+// /////////////根据图片设置尺寸，防止内容抖动///////////////// //
+// 请求携带尺寸的图片
+let requestImg = [
+  {
+    src: './img/file_4963928.webp',
+    ph: './img/placeholder/file_4963928.webp',
+    w: 450,
+    h: 512,
+  },
+  {
+    src: './img/file_4963929.webp',
+    ph: './img/placeholder/file_4963929.webp',
+    w: 432,
+    h: 512,
+  },
+  {
+    src: './img/file_4963930.webp',
+    ph: './img/placeholder/file_4963930.webp',
+    w: 512,
+    h: 478,
+  },
+  {
+    src: './img/file_4963931.webp',
+    ph: './img/placeholder/file_4963931.webp',
+    w: 323,
+    h: 512,
+  },
+  {
+    src: './img/file_4963932.webp',
+    ph: './img/placeholder/file_4963932.webp',
+    w: 512,
+    h: 480,
+  },
+  {
+    src: './img/file_4963933.webp',
+    ph: './img/placeholder/file_4963933.webp',
+    w: 353,
+    h: 512,
+  },
+  {
+    src: './img/file_4963934.webp',
+    ph: './img/placeholder/file_4963934.webp',
+    w: 346,
+    h: 512,
+  },
+  {
+    src: './img/file_4963935.webp',
+    ph: './img/placeholder/file_4963935.webp',
+    w: 394,
+    h: 512,
+  },
+  {
+    src: './img/file_4963936.webp',
+    ph: './img/placeholder/file_4963936.webp',
+    w: 272,
+    h: 512,
+  },
+];
+// 创建图片
+function createImg2() {
+  let frag = document.createDocumentFragment();
+  for (let i of requestImg) {
+    let img = document.createElement('img');
+    // placeholder 地址
+    img.src = `${i.ph}`;
+    // 图片真实地址
+    img.setAttribute('data-src', `${i.src}`);
+    // 根据请求的图片尺寸动态设置尺寸
+    img.style.width = `${i.w}px`;
+    img.style.height = `${i.h}px`;
+    frag.append(img);
+  }
+  frag.append(document.createElement('hr'));
+  return frag;
+}
+let images2 = createImg2();
+document.body.append(images2);
+
+// /////////////懒加载的实现///////////////// //
+
+// /////////////懒加载的实现///////////////// //
 // 选中所有懒加载的图片
 let allImg = document.querySelectorAll('img[data-src]');
 // 接受一个 img 元素作为参数
@@ -157,7 +193,6 @@ function loadImg(img) {
 // allImg.forEach((val) => {
 //   loadImg(val);
 // });
-
 // 创建一个 observer 对象，接受一个回调
 let observer = new IntersectionObserver((entries, observer) => {
   // IntersectionObserverEntry 参数
